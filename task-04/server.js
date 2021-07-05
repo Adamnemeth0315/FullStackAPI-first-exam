35b98806a408fe0e01f5270bb8c83d6baba72a64
Adamnemeth0315 <adam.nemeth.0315@gmail.com>
2021-07-05 15:09:08 +0200

    task-4 done

--- task-04/server.js
+++ task-04/server.js
@@ -3,15 +3,25 @@
  * függvényét.
  */
 
-// 2. Definiáld a port értékét 8080 -ra a port változóban.
+const http = require('http');
+const {getAll} = require('./jsonDB');
 
+// 2. Definiáld a port értékét 8080 -ra a port változóban.
+const port = 8080;
 /**
  * 3. Hozz létre egy http szervert.
  * A szerver get kérés esetén a getAll függvény segítségével lekéri a listát, 
  * majd beállítja a headert, hogy json a tartalom típusa, 
  * végül visszaküldi a kliensnek a listában tárolt adatokat.
  */
-
+const server = http.createServer(async (req, res) => {
+    const list = await getAll();
+    res.setHeader('Content-Type', 'aplication/json');
+    res.end(JSON.stringify(list));
+})
 /**
  * 4. Állítsd be, hogy a szerver figyelje a port változóban definiált portot.
  */ 
+server.listen(port, () => {
+    console.log(`Server is running: http://127.0.0.1:${port}`);
+});
